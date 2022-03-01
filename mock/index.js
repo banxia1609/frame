58a879e4863f83198eaa4f7fb53a23d1bfab683a
banxia1609 <362452476@qq.com>
2022-03-01 12:11:28 +0800

    vuecli3.0+vue2.0+elementui框架

--- mock/index.js
+++ mock/index.js
@@ -4,13 +4,12 @@ let Mock = require("mockjs"); //引入mock模块
 
 var http = require('http');
 // var server = http.createServer();
-console.log(http, http.ServerResponse)
-let express = require('express')
-console.log(express, 'express')
+// console.log(http, http.ServerResponse)
+// let express = require('express')
+// console.log(express, 'express')
 
 
 Mock.mock('/passport/login', function (options) {
-    console.log('进来了')
     return {
         status: 200,
         data: {
